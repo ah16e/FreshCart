8f54a25545bbd090884bd4346d561e3a0768f22c
Ahmed Mohamed <sabry9800@gmail.com>
2024-08-25 15:02:10 +0300

    FreshCart

--- src/Components/Allorders/Allorders.jsx
+++ src/Components/Allorders/Allorders.jsx
@@ -13,12 +13,14 @@ export default function Allorders() {
 
 
   return <>
-     <div className="pt-16 pb-24 text-center">
+   <div className="pt-24 pb-24">
+   <div className="pt-16 pb-24 rounded-md border border-green-500 text-center">
     <h1 className="text-3xl">your payment process is successful <i class="fa-regular text-green-600 fa-circle-check"></i></h1>
       <div className="pt-4">
     <button className='bg-main p-3 rounded-md '><NavLink to={'/'}>Home</NavLink></button>
       </div>
      </div>
+   </div>
   
   </>
 }
